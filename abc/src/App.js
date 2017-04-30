5f02ecf4067f3c78728b129b2d1d745311b34acd
taryongyuthecho <taryongyuth@gmail.com>
2017-04-30 15:33:37 +0700

    Commit !!!

--- abc/src/App.js
+++ abc/src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Route path="/about" component={About}/>
         </p>
       </div>
+      </Router>
     );
   }
 }
